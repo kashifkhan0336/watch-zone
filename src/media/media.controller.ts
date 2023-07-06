b2a950ae9e296e9da678871e35292388c0ba124b
Kashif Khan <kashifkhan0336@gmail.com>
2023-07-06 03:14:16 +0000

    a

--- src/media/media.controller.ts
+++ src/media/media.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+
+import { Body, Controller, Get, Param, Post, UseGuards, UsePipes } from '@nestjs/common';
 
 @Controller('media')
-export class MediaController {}
+export class MediaController {
+    @Get("/movie/:id")
+    getMovie(@Param() params: any): string {
+        return params.id
+    }
+
+    @Get("/movies/")
+    getMovies(): string[] {
+        return ["Dark Knight", "Batman begins"]
+    }
+
+    @Get("/movies/:genre")
+    getMoviesByGenre(@Param() params: any): Array<{ name: string }> {
+        if (params.genre == "action") {
+            return [
+                {
+                    name: "Dark Knight"
+                }
+            ]
+        }
+        if (params.genre == "comedy") {
+            return [
+                {
+                    name: "Mr Bean"
+                }
+            ]
+        }
+    }
+}
